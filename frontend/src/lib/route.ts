5a5cf540e6dbd185bb96e8941d50cb24936bae80
Sourabh Mandal <19mandal97@gmail.com>
2025-06-22 16:25:55 +0530

    chore: segregate frontend

--- frontend/src/lib/route.ts
+++
@@ -1,2 +0,0 @@
-export const ROOT = '/';
-export const AUTH = '/auth';
--- frontend/src/server/gemini/index.ts
+++
@@ -1,6 +0,0 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-
-const llm = new ChatGoogleGenerativeAI({
-  model: "gemini-2.0-flash",
-  temperature: 0
-});
